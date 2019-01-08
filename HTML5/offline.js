7e2db1d9408c11bc269ed55226fe39f6a26431dd
richardfructos <46432000+richardfructos@users.noreply.github.com>
2019-01-07 23:16:19 -0200

    Add files via upload

---
+++ HTML5/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1546908240,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg_2-sheet0.png",
+		"images/bt_play2-sheet0.png",
+		"images/nome2-sheet0.png",
+		"images/fundo2-sheet0.png",
+		"images/player_2-sheet0.png",
+		"images/frutas-sheet0.png",
+		"images/obj_destroy-sheet0.png",
+		"images/btn_home-sheet0.png",
+		"images/btn_restart-sheet0.png",
+		"images/pf-sheet0.png",
+		"images/particles.png",
+		"media/buttons (1).ogg",
+		"media/colect.ogg",
+		"media/morte.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
